33985ccb675aec534dfeacf21046ddaf94dc70bb
Artem Gilmudinov <guliash92@gmail.com>
2018-12-18 23:22:22 +0300

    moving toFullScreen;

--- index.js
+++ index.js
@@ -529,6 +529,12 @@ export default class VideoPlayer extends Component {
       </View>
     );
   }
+
+  toFullScreen() {
+    if (this.player) {
+      this.player.presentFullscreenPlayer();
+    }
+  }
 }
 
 VideoPlayer.propTypes = {
